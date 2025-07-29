cd46e780aaab2f8697faea63bdba01d982742d11
Tezzy <zacharyeverett.00@gmail.com>
2025-07-29 13:12:50 +1000

    basic rendering of image to ppm file

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<fstream>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+int main() {
+    std::ofstream MyFile("output.ppm");
+    MyFile << "P3" << std::endl;
+    MyFile << WIDTH << " " << HEIGHT << std::endl;
+    MyFile << "255" << std::endl;
+    for(int i = 0; i < HEIGHT; ++i) {
+        for(int j = 0; j < WIDTH; ++j) {
+            int r = i*256/HEIGHT;
+            int g = j*256/WIDTH;
+            int b = 255;
+            MyFile << r << " " << g << " " << b << std::endl;
+        }
+    }
+
+    MyFile.close();
+    return 0;
+}
