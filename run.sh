bad76d5f2ca78d332f4fd38bec771169b7138ba5
Tezzy <zacharyeverett.00@gmail.com>
2025-07-31 14:56:21 +1000

    fixed cpp standard for mac in run.sh file and fixed formatting in sphere file

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-g++ src/main.cpp src/Ray.cpp src/Sphere.cpp src/Hit.cpp src/Scene.cpp -o main
+g++ -std=c++17 src/main.cpp src/Ray.cpp src/Sphere.cpp src/Hit.cpp src/Scene.cpp -o main
 ./main
--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -4,40 +4,40 @@
 #include <cmath>
 
 Hit Sphere::getIntersection(Ray ray) {
-            std::array<float, 3> difference = {
-                ray.position[0] - position[0],
-                ray.position[1] - position[1],
-                ray.position[2] - position[2]
-            };
-            float a = ray.direction[0] * ray.direction[0] + ray.direction[1] * ray.direction[1] + ray.direction[2] * ray.direction[2];
-            float b = 2 * (ray.direction[0] * difference[0] + ray.direction[1] * difference[1] + ray.direction[2] * difference[2]);
-            float c = difference[0] * difference[0] + difference[1] * difference[1] + difference[2] * difference[2] - radius * radius;
+    std::array<float, 3> difference = {
+        ray.position[0] - position[0],
+        ray.position[1] - position[1],
+        ray.position[2] - position[2]
+    };
+    float a = ray.direction[0] * ray.direction[0] + ray.direction[1] * ray.direction[1] + ray.direction[2] * ray.direction[2];
+    float b = 2 * (ray.direction[0] * difference[0] + ray.direction[1] * difference[1] + ray.direction[2] * difference[2]);
+    float c = difference[0] * difference[0] + difference[1] * difference[1] + difference[2] * difference[2] - radius * radius;
 
-            float discriminant = b * b - 4 * a * c;
+    float discriminant = b * b - 4 * a * c;
 
-            if(discriminant < 0) {
-                return Hit::emptyHit();
-            }
-            
-            float t = (-b - sqrt(discriminant)) / (2 * a);
+    if(discriminant < 0) {
+        return Hit::emptyHit();
+    }
+    
+    float t = (-b - sqrt(discriminant)) / (2 * a);
 
-            if(t < 0.00001) {
-                return Hit::emptyHit();
-            }
+    if(t < 0.00001) {
+        return Hit::emptyHit();
+    }
 
-            Hit hit;
-            hit.t = t;
-            hit.sphere = this;
-            hit.position = {
-                ray.position[0] + ray.direction[0] * t,
-                ray.position[1] + ray.direction[1] * t,
-                ray.position[2] + ray.direction[2] * t
-            };
-            hit.normal = {
-                hit.position[0] - position[0],
-                hit.position[1] - position[1],
-                hit.position[2] - position[2]
-            };
+    Hit hit;
+    hit.t = t;
+    hit.sphere = this;
+    hit.position = {
+        ray.position[0] + ray.direction[0] * t,
+        ray.position[1] + ray.direction[1] * t,
+        ray.position[2] + ray.direction[2] * t
+    };
+    hit.normal = {
+        hit.position[0] - position[0],
+        hit.position[1] - position[1],
+        hit.position[2] - position[2]
+    };
 
-            return hit;
-        }
+    return hit;
+}
