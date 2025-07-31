6692be4d9b57aa4edb0942c3f37d4cc822012002
tezzy <zacharyeverett.00@gmail.com>
2025-08-01 01:19:22 +1000

    nice 1000spp 5 bounce render done of basic scene

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ const int WIDTH = 800;
 const int HEIGHT = 600;
 const float FOV = 90; // degrees
 const int BOUNCES = 5;
-const int SAMPLESPERPIXEL = 100;
+const int SAMPLESPERPIXEL = 1000;
 
 const char* FILENAME = "output.ppm";
 
